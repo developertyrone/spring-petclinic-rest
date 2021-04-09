42aa48764d360cf878297277a019fc178291664b
Tyrone <developertyrone@gmail.com>
2021-04-09 16:25:13 +0800

    fix

--- src/main/java/org/springframework/samples/petclinic/rest/HeaderRestController.java
+++ src/main/java/org/springframework/samples/petclinic/rest/HeaderRestController.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 
 @RestController
@@ -17,13 +20,11 @@ import java.io.IOException;
 public class HeaderRestController {
     private static final Logger log = LoggerFactory.getLogger(ExceptionRestController.class);
 
-  
-
     @RequestMapping(value = "", method = RequestMethod.GET, produces = "text/plain")
     @ResponseBody
-    public String makeExceptionLog(@RequestHeader Map<String, String> headers) {
+    public ResponseEntity makeExceptionLog(@RequestHeader Map<String, String> headers) {
         headers.forEach((key, value) -> {
-            LOG.info(String.format("Header '%s' = %s", key, value));
+            log.info(String.format("Header '%s' = %s", key, value));
         });
 
         return new ResponseEntity<String>(String.format("Listed %d headers", headers.size()), HttpStatus.OK);
