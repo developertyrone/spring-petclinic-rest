8f81bf3ef9ecc89e023675586deb70269e846c3c
Tyrone <developertyrone@gmail.com>
2021-04-09 17:21:03 +0800

    fix

--- src/main/java/org/springframework/samples/petclinic/rest/HeaderRestController.java
+++ src/main/java/org/springframework/samples/petclinic/rest/HeaderRestController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +23,7 @@ public class HeaderRestController {
 
     @RequestMapping(value = "", method = RequestMethod.GET, produces = "text/plain")
     @ResponseBody
-    public String makeExceptionLog(@RequestBody String body, @RequestHeader Map<String, String> headers) {
+    public String makeExceptionLog(@RequestHeader HttpHeaders headers) {
         headers.forEach((key, value) -> {
             log.info(String.format("Header '%s' = %s", key, value));
         });
